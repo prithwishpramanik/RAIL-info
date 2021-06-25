1026434db769e1f1e6e6616ddad2206deb7b357a
Prithwish Pramanik <prithwishpramanik462@gmail.com>
2021-06-25 09:58:17 +0530

    Modal 2 up and functional
    Modal 2 up and functional

--- js/app.js
+++ js/app.js
@@ -87,12 +87,37 @@ console.log(number);
 
                 for(let i=0;i<response.length;i++){
 
+
                     blobnumber = blobnumber +`<tr>
 <td>${response[i].train_num}</td>
 <td>${response[i].name}</td>
 <td>${response[i].train_from}</td>
 <td>${response[i].train_to}</td>
 <td>${response[i].data.arriveTime}</td>
+<td>${response[i].data.classes}</td>
+
+<td><table class="table">
+<th>Sunday</th>
+<th>Monday</th>
+<th>Tuesday</th>
+<th>Wednesday</th>
+<th>Thursday</th>
+<th>FriDay</th>
+<th>Saturday</th>
+<tr>
+<td>${response[i].data.days.Sun}</td>
+<td>${response[i].data.days.Mon}</td>
+<td>${response[i].data.days.Tue}</td>
+<td>${response[i].data.days.Wed}</td>
+<td>${response[i].data.days.Thu}</td>
+<td>${response[i].data.days.Fri}</td>
+<td>${response[i].data.days.Sat}</td>
+</tr>
+</tr>
+
+</table></td>
+
+
 </tr>`;
 
                     $('#display2').html(`<table class="table table-striped">
@@ -102,6 +127,8 @@ console.log(number);
                                 <th>Departing Station</th>
                                 <th>Destination Station</th>
                                 <th>Arrival Time</th>
+                                <th>Travel Class Availabale</th>
+                                <th>Days On Which The Trains Are Available</th>
                             </tr>
                             ${blobnumber}
 
@@ -112,12 +139,19 @@ console.log(number);
 
 
                     console.log(response);
-                   let weekdays=["Fri","Mon","Sat","Sun","Thu","Tue","Tue","Wed"];
+                    console.log(response[i].data.days);
 
-                   let day='response[i].data.days';
-                   const obj = JSON.parse(day);
-                   console.log();
-                    console.log(weekdays);
+
+
+
+
+
+
+
+
+
+
+
 
                 }
             });
@@ -125,5 +159,5 @@ console.log(number);
 
 
 
-    })
+    })//Details from Train Number//
 });
